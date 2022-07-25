e0909b16a3eaf300f79e4bdf3bf81df5aa8c7dfc
Damilola Ojo <tdami262@gmail.com>
2022-07-25 05:13:17 +0100

    Add validation to Category & Location Model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   has_many :listings
   has_many :locations
+
+  validates :name, presence: true
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,6 @@
 class Location < ApplicationRecord
   has_many :listings
   belongs_to :category
+
+  validates :name, presence: true
 end
