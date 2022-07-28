30f5c066d1cd8313b4ce045249b396d5d868390d
Damilola Ojo <tdami262@gmail.com>
2022-07-28 03:48:31 +0100

    Update root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'listings/name'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "listings#index"
 end
