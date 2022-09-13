f3766cb626881f9ec5d9ffe2b196f485efb21289
Damilola Ojo <tdami262@gmail.com>
2022-09-13 06:37:46 +0100

    Generate Reservation Model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,6 +3,7 @@ class Listing < ApplicationRecord
   belongs_to :category
   has_many :amenities
   has_many :reviews
+  has_many :reservations
 
   validates :name, :description, :capacity, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_25_034154) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_13_050711) do
   create_table "amenities", force: :cascade do |t|
     t.string "name", null: false
     t.integer "listing_id", null: false
@@ -45,6 +45,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_25_034154) do
     t.index ["category_id"], name: "index_locations_on_category_id"
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.integer "listing_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["listing_id"], name: "index_reservations_on_listing_id"
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.text "content", null: false
     t.integer "listing_id", null: false
@@ -57,5 +64,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_25_034154) do
   add_foreign_key "listings", "categories"
   add_foreign_key "listings", "locations"
   add_foreign_key "locations", "categories"
+  add_foreign_key "reservations", "listings"
   add_foreign_key "reviews", "listings"
 end
