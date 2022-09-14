b1aed0a4ea4d7c60fb56a8444bf7a7fb3e3179c3
Damilola Ojo <tdami262@gmail.com>
2022-09-14 12:19:46 +0100

    Add tests for Listing controller actions

--- test/controllers/listings_controller_test.rb
+++ test/controllers/listings_controller_test.rb
@@ -1,8 +1,19 @@
 require "test_helper"
 
 class ListingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get name" do
-    get listings_name_url
+
+  test "should get index" do
+    get listings_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_listing_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_listing_url(Listing.first)
     assert_response :success
   end
 end
