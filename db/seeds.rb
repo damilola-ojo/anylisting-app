f997308f29b777b933bdbd1a5d9d6a6fe70a8d1a
Damilola Ojo <tdami262@gmail.com>
2022-09-15 17:21:54 +0100

    Add Faker Gem & seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+10.times do |n|
+  category = Category.new(name: Faker::App.name)
+  location = Location.create(name: Faker::Address.city, category: category)
+
+  description = Faker::Lorem.paragraph(random_sentences_to_add: 4)
+
+  Listing.create(name: "Listing #{n}",
+                 description: description,
+                 capacity: "#{n}",
+                 location: location,
+                 category: category)
+end
